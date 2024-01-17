a64816da0d26a02aef75b3b26fec430e5315a29c
Thiago_Reis <reist36@gmail.com>
2024-01-17 17:17:07 -0300

    Implementation: findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.Iterator;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,10 +15,16 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===== TEST 1: seller findByID =====");
-		
 		Seller seller = sellerDao.findById(8);
-		
 		System.out.println(seller);
+		
+		System.out.println();
+		System.out.println("===== TEST 2: seller findByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		list.forEach(System.out::println);
+		
 
 	}
 
