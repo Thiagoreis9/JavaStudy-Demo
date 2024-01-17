d89a821ad114f2d6bfe14d0989d93a334877544d
Thiago_Reis <reist36@gmail.com>
2024-01-17 17:36:42 -0300

    Implementation: findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Iterator;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -22,8 +21,13 @@ public class Program {
 		System.out.println("===== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
 		list.forEach(System.out::println);
+		
+		System.out.println();
+		System.out.println("===== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		list.forEach(System.out::println);
+		System.out.println("Seller quantity: " + list.size());
 		
 
 	}
