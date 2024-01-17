e20ac5c648d5747429c49616ad71ae368d70524a
Thiago_Reis <reist36@gmail.com>
2024-01-17 16:37:07 -0300

    Create: FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department d1 = new Department(1, "Computer");
-		
-		System.out.println(d1);
-		
-		Seller s1 = new Seller(1, "Thiago", "reist36@gmail", new Date(), 3000.00, d1);
-		
-		System.out.println(s1);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(9);
+		
+		System.out.println(seller);
 
 	}
 
