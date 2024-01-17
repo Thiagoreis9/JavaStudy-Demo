c997b0fce1371579977a8c1eb4dd5149ff61dc21
Thiago_Reis <reist36@gmail.com>
2024-01-17 15:28:59 -0300

    Create: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department d1 = new Department(1, "Computer");
 		
 		System.out.println(d1);
+		
+		Seller s1 = new Seller(1, "Thiago", "reist36@gmail", new Date(), 3000.00, d1);
+		
+		System.out.println(s1);
 
 	}
 
